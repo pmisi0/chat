745a3ae4eb7b0290fce9f90ceeae05c69f61f8bb
pmisi0 <58913794+pmisi0@users.noreply.github.com>
2026-07-11 08:02:34 +0200

    Chat.html
    Chat

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => io.emit('chat message', msg));
+});
+
+http.listen(process.env.PORT || 3000);
